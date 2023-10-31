39f8bac50aff4d1295f13dec7913dcc4a7bf2510
Kike <kryke10@gmail.com>
2023-10-31 16:26:19 +0100

    cuidao

--- Blog/modelos/MensajesDAO.php
+++ Blog/modelos/MensajesDAO.php
@@ -7,20 +7,50 @@ class MensajesDAO {
         $this->conn = $conn;
     }
 
-    public function getById($id):Mensaje|null {
-        if(!$result = $this->conn->query("SELECT * FROM mensajes WHERE id = $id"))
-        {
+    public function getById($id):Mensaje {
+       // if(!$result = $this->conn->query("SELECT * FROM mensajes WHERE id = $id"))
+       if(!$stmt =  $this->conn->prepare("SELECT * FROM mensajes WHERE id = $id")){
+       
             echo "Error en la SQL: " . $this->conn->error;
         }
-        if($result->num_rows == 1){
+       /*  if($result->num_rows == 1){
             $mensaje = $result->fetch_object(Mensaje::class);
             return $mensaje;
         }
         else{
             return null;
         }
+ */
 
+    //Asociar las variables a las interrogaciones (parametros)
+    $stmt->bind_param('i', $id);
+    //Ejecutamos la SQL
+    $stmt->execute();
+    //Obtener el objeteo mysql_result
+    $result = $stmt->get_result();
+
+    //Si ha encontrado algun resultado devolvemos un objeto de la clase Mensaje, sino null
+    if($result->num_rows == 1){
+        $mensaje = $result->fetch_object(Mensaje::class);
     }
+    }
+    public function getAll():array {
+        if(!$stmt =  $this->conn->prepare("SELECT * FROM mensajes")){
+            echo "Error en la SQL: " . $this->conn->error;
+    }
+    //Ejecutamos la SQL
+    $stmt->execute();
+    //Obtener el objeteo mysql_result
+    $result = $stmt->get_result();
 
+    $$array_mensajes = array();
+// $array_mensajes = array();
+//$num_fil a= $result_
+
+    while($mensaje = $result ->fetch_object(Mensaje::class)){
+        $array_mensajes[] = $mensaje;
+    }
+    return $array_mensajes;
+    }
 }
 ?>
